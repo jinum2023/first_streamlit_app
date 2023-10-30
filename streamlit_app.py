ef5b9dfff063b1571f1d29c8a9a154ce6417effd
jinum2023 <149435420+jinum2023@users.noreply.github.com>
2023-10-30 14:24:41 -0700

    Menu updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
-streamlit.title('My Parents new healthy Dinner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & rocket Smoothie')
-streamlit.text('Hard-boiled Free-range Egg')
+streamlit.title('My Mom's new healthy Dinner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & rocket Smoothie')
+streamlit.text('🐔 Hard-boiled Free-range Egg')
+streamlit.text('🥑 🍞 Avacado toast')
