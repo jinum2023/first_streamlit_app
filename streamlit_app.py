834cd77ffdbe02c46d353d77d6a892f523332f0a
jinum2023 <149435420+jinum2023@users.noreply.github.com>
2023-10-30 14:20:00 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents new healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & rocket Smoothie')
+streamlit.text('Hard-boiled Free-range Egg')
